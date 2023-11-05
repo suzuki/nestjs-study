05ee4beb7f96e9e083b9f194e99497cb1d4ad9a5
Norio Suzuki <norio.suzuki@gmail.com>
2023-11-06 08:26:18 +0900

    Add e2e test

---
+++ test/cats/cats.e2e-spec.ts
@@ -0,0 +1,35 @@
+import * as request from 'supertest';
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { CatsModule } from '../../src/cats/cats.module';
+import { CatsService } from '../../src/cats/cats.service';
+
+describe('Cats', () => {
+  let app: INestApplication;
+  let catsService = { findAll: () => ['test'] };
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [CatsModule],
+    })
+      .overrideProvider(CatsService)
+      .useValue(catsService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/GET cats', () => {
+    return request(app.getHttpServer())
+      .get('/cats')
+      .expect(200)
+      .expect({
+        data: catsService.findAll(),
+      });
+  });
+});
